c1ea22f5bd2c34839e7aa93b05647d0edc0b9603
Jack <archjacsont@gmail.com>
2021-10-15 13:36:48 -0400

    Add XMonad, and remove blank lines on parse

--- parse.sh
+++ parse.sh
@@ -3,7 +3,7 @@ end_pacman="pacman --"
 begin_aur="aur ++"
 end_aur="aur --"
 [ -f /usr/bin/paru ] || pacman -S paru
-sed -n "/^$begin_pacman/,/^$end_pacman/p" ./pkglist | sed "s/$begin_pacman//"  | sed "s/$end_pacman//" |sudo  pacman -S -
-sed -n "/^$begin_aur/,/^$end_aur/p" ./pkglist | sed "s/$begin_aur//"  | sed "s/$end_aur//" | paru -S -
+sed -n "/^$begin_pacman/,/^$end_pacman/p" ./pkglist | sed "s/$begin_pacman//"  | sed "s/$end_pacman//" | sed '/^[[:space:]]*$/d' | sudo  pacman -S -
+sed -n "/^$begin_aur/,/^$end_aur/p" ./pkglist | sed "s/$begin_aur//"  | sed "s/$end_aur//" | sed '/^[[:space:]]*$/d' | paru -S -
 
 
